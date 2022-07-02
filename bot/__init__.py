bec77a8122ad8ac105f87ff62e53b99ab66ace02
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-07-02 10:54:42 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,11 +3,11 @@ import asyncio
 from pyrogram import Client
 from dotenv import load_dotenv
 
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_id = int(os.environ.get("7931165"))
+api_hash = os.environ.get("5a2e46d1e6deb1456c75aa743bc8e0e6")
+bot_token = os.environ.get("5159774583:AAHaIX-f9DmCZWsbAH8X-j6B17BCpM8K6hI")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = list(set(int(x) for x in os.environ.get("-1001749500356").split()))
 ffmpeg = os.environ.get("FFMPEG", "")
 
 app = Client("nirusakibot", api_id=api_id, api_hash=api_hash, bot_token=bot_token, workers=2)
