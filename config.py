062dfdb93445dee72f8a626f284364293cfa269d
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2022-11-09 21:02:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-BOT_TOKEN = getenv("BOT_TOKEN", "5159774583:AAHaIX-f9DmCZWsbAH8X-j6B17BCpM8K6hI")
-API_ID = int(getenv("API_ID", "7931165"))
-API_HASH = getenv("API_HASH", "5a2e46d1e6deb1456c75aa743bc8e0e6")
-SUDO_USERS = getenv("SUDO_USERS", "1615607413"))
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+SUDO_USERS = getenv("SUDO_USERS", ""))
