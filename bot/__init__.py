dd2d081ea9e9952ad773289760ff341690cd342d
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2022-11-09 21:10:38 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -7,7 +7,7 @@ api_id = os.environ.get("8978848")
 api_hash = os.environ.get("24ce3cff2d32cf529df1c0018e28d6cf")
 bot_token = os.environ.get("5562438875:AAHKfFy6PdRkrEBqlt6w2mqghtlOKB19kSo")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("-1001816242004").split()))
+sudo_users = os.environ.get("-1001816242004 1995886602")
 ffmpeg = os.environ.get("FFMPEG", "ffmpeg -i '''{}''' -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? '''{}''' -y")
 
 app = Client("nirusakibot", api_id=api_id, api_hash=api_hash, bot_token=bot_token, workers=2)
